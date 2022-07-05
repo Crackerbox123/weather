0e1a6d78f541f8e14f2bebd1fd7b8b72853733bb
Kal O <koestreicher4@gmail.com>
2022-07-04 20:49:06 -0500

    display function logic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,10 @@
 var userFormEl = document.querySelector("#search-form");
 var searchInputEl = document.querySelector("#search");
-var latitude = "";
-var longitude = "";
+var dateHeader = document.querySelector("#date");
+var tempCurrent = document.querySelector("#temp");
+var windCurrent = document.querySelector("#wind");
+var humidCurrent = document.querySelector("#humidity");
+var uvCurrent = document.querySelector("#uv");
 
 // Geolocation API
 // get input data from the submit form, saves as location, sends to getLocationData
@@ -18,28 +21,48 @@ var formSubmitHandler = function(event) {
 
 // function to get location data
 var getLocationData = function(location) {
+    // apiurl, with location parameter from fromSubmitHandler
     var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&appid=c7c0c5deb67324c42122e65149bc7cda";
     fetch(apiUrl).then(function(response){
     response.json().then(function(data) {
+        console.log(data);
         // latitude variable set
         console.log(data[0].lat);
         var latitude = data[0].lat;
         // long variable set
         console.log(data[0].lon);
         var longitude = data[0].lon;
+
+
+        // lat + lon params sent to getWeather function4
+
         getWeather(latitude, longitude)
     });
 });
 };
 
 var getWeather = function(latitude, longitude) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=c7c0c5deb67324c42122e65149bc7cda"
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=c7c0c5deb67324c42122e65149bc7cda"
     fetch(apiUrl).then(function(response){
         response.json().then(function(data) {
-            console.log(data)
+            // captures response data, names it, and sends to dispalyWeather function as a parameter
+            var weather = data;
+            displayWeather(weather);
         });
     });
 };
+
+var displayWeather = function(weather) {
+
+    console.log(weather);
+    tempCurrent.textContent = weather.current.temp;
+    console.log(weather.current.temp);
+    console.log(weather.current.wind_speed);
+    console.log(weather.current.humidity);
+    console.log(weather.current.uvi);
+}
+
+
 
 
 // 
